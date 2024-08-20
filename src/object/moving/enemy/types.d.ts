454dedce0c7d1ec6dc7b8514aca1f6e3a3cdf2d6
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-08-20 23:40:35 +0900

    feat:enemy 인터페이스 정의 및 새로운 속성 추가
    * gold, health, power

--- src/object/moving/enemy/types.d.ts
+++ src/object/moving/enemy/types.d.ts
@@ -1,1 +1,10 @@
 export type EnemyType = "slime" | "goblin";
+
+export interface IEnemy {
+    className: string;
+    id: string; 
+    type: EnemyType; 
+    gold:number; 
+    health:number;
+    power:number;
+  }
