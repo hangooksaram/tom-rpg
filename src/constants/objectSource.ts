b1f5bda8981cb1dd02c19984cdb8c8e6ecf87bd0
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-10-08 21:53:32 +0900

    feat:player source 상수 추가

--- src/constants/objectSource.ts
+++ src/constants/objectSource.ts
@@ -2,7 +2,10 @@ const SOURCE_PATH = '/public/images';
 
 export const objectSource = {
     PLAYER: {
-        PAUSE:`${SOURCE_PATH}/player/pause.png`
+        PAUSE:`${SOURCE_PATH}/player/pause.png`,
+        RUN1:`${SOURCE_PATH}/player/run1.png`,
+        RUN2:`${SOURCE_PATH}/player/run2.png`,
+        RUN3:`${SOURCE_PATH}/player/run3.png`,
     },
     GOBLIN:`${SOURCE_PATH}/monster/goblin.png`,
     SLIME:`${SOURCE_PATH}/monster/slime.png`,
