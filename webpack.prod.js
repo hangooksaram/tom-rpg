8a326b96336be8e8e8de673159e4e5b7902129b3
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-10-22 15:41:25 +0900

    chore:webpack.prod.config 변경

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,6 +25,10 @@ module.exports = {
         type: 'asset/resource',
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
