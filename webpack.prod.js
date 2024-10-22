0e7c309a43d03023727ba9498a9f9eca02902f62
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-10-22 17:06:43 +0900

    chore:font rule 순서 변환

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,12 +25,12 @@ module.exports = {
         type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
       {
         test: /\.html/,
