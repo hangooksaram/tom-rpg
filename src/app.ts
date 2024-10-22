a0102323b37c018e803112f15d2dac18f83156b8
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-10-22 11:22:28 +0900

    asset:폰트 추가

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { game } from './game/game';
 import '/reset.css';
-import './style.css';
+import './style/style.css';
+import './style/ui.css';
 
 game.initialize();
--- webpack.dev.js
+++ webpack.dev.js
@@ -24,6 +24,10 @@ module.exports = {
         type: 'asset/resource',
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
