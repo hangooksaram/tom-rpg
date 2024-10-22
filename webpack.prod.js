7b4ac6814fa0d2c28019b5f5024a1f8c401e5a5c
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-10-22 17:16:39 +0900

    chore:webpack.prod.confg font rule 에 url-loader 추가

--- webpack.prod.js
+++ webpack.prod.js
@@ -30,7 +30,9 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        use: {
+          loader: 'url-loader',
+        },
       },
       {
         test: /\.html/,
